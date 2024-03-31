7ad2ff62e2925fdbacaae42364717e1bd552b54e
DevAdedeji <adedejitewogbade2@gmail.com>
2024-03-31 14:38:56 +0100

    Done

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@
     <div class="flex flex-col items-center gap-4">
       <h1 class="text-xl text-center underline">Components</h1>
       <ul class="flex flex-col items-center gap-2 list-disc">
-        <li><RouterLink to="/modal">Modal</RouterLink></li>
+        <li><RouterLink to="/modal" class="hover:text-blue-500">Modal</RouterLink></li>
       </ul>
     </div>
   </main>
